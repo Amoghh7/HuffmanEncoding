3dd30dc6c24f2485eeb9cfcecd45f8a0dd2e675b
Amogh Joshi <110009343+Amoghh7@users.noreply.github.com>
2023-06-16 08:59:34 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,7 @@
+class Tree():
+    # Class Tree is used to create the huffman tree which will be traversed.
+    def __init__(self, parent, left_child=None, right_child=None):
+        self.parent = parent
+        self.left_child = left_child
+        self.right_child = right_child
+
